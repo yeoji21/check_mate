2c0078315561d13f58cb5d2a3a1900eb4eed2875
여지원 <yooara0508@naver.com>
2022-11-03 23:59:29 +0900

    새 프로젝트 생성

---
+++ src/test/java/com/checkyourgoal/checkmate/CheckMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkyourgoal.checkmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
