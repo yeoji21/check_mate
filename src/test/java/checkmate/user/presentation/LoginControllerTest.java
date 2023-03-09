5d20b61798e281ad7cd4b14d85d1abc236755976
여지원 <yooara0508@naver.com>
2023-03-09 16:26:37 +0900

    Chore : 로그인 API rest docs 문서화

--- src/test/java/checkmate/user/presentation/LoginControllerTest.java
+++ src/test/java/checkmate/user/presentation/LoginControllerTest.java
@@ -60,11 +60,92 @@ class LoginControllerTest extends ControllerTest {
         verify(loginService).logout(any(Long.class));
     }
 
+    @WithMockAuthUser
+    @Test
+    @DisplayName("구글 로그인 API")
+    void loginGoogle() throws Exception {
+        GoogleLoginDto loginDto = new GoogleLoginDto("providerId", "test FcmToken");
+        LoginTokenResponse response = createLoginTokenResponse();
+
+        given(loginService.login(any())).willReturn(response);
+
+        mockMvc.perform(post("/login/google")
+                        .contentType(APPLICATION_JSON)
+                        .with(csrf())
+                        .content(objectMapper.writeValueAsString(loginDto)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(response)))
+                .andDo(document("login-google",
+                        loginRequestFieldsSnippet(),
+                        loginResponseFieldsSnippet()
+                ));
+    }
+
+    @WithMockAuthUser
+    @Test
+    void 네이버_로그인_성공_테스트() throws Exception {
+        NaverLoginDto loginDto = new NaverLoginDto("providerId", "test FcmToken");
+        LoginTokenResponse response = createLoginTokenResponse();
+
+        when(loginService.login(any())).thenReturn(response);
+
+        mockMvc.perform(post("/login/naver")
+                        .contentType(APPLICATION_JSON)
+                        .with(csrf())
+                        .content(objectMapper.writeValueAsString(loginDto)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(response)))
+                .andDo(document("login-naver",
+                        loginRequestFieldsSnippet(),
+                        loginResponseFieldsSnippet()
+                ));
+    }
+
+    @WithMockAuthUser
+    @Test
+    void 카카오_로그인_성공_테스트() throws Exception {
+        KakaoLoginDto loginDto = new KakaoLoginDto("providerId", "testFcmToken");
+        LoginTokenResponse response = createLoginTokenResponse();
+
+        given(loginService.login(any())).willReturn(response);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/login/kakao").contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginDto))
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(response)))
+                .andDo(document("login-kakao",
+                        loginRequestFieldsSnippet(),
+                        loginResponseFieldsSnippet()
+                ));
+    }
+
+    private ResponseFieldsSnippet loginResponseFieldsSnippet() {
+        return responseFields(
+                fieldWithPath("accessToken").type(JsonFieldType.STRING).description("로그인 성공 후 발급하는 accessToken"),
+                fieldWithPath("refreshToken").type(JsonFieldType.STRING).description("로그인 성공 후 발급하는 refreshToken")
+        );
+    }
+
+    private RequestFieldsSnippet loginRequestFieldsSnippet() {
+        return requestFields(
+                fieldWithPath("providerId").type(JsonFieldType.STRING).description("로그인 시도하는 소셜 고유 아이디"),
+                fieldWithPath("fcmToken").type(JsonFieldType.STRING).description("로그인한 기기의 fcmToken")
+        );
+    }
+
     private ResponseFieldsSnippet tokenReissueResponseFieldsSnippet() {
         return responseFields(
                 fieldWithPath("refreshToken").type(JsonFieldType.STRING).description("새로운 refresh token"),
                 fieldWithPath("accessToken").type(JsonFieldType.STRING).description("새로운 access token")
         );
+    }
+
+    private LoginTokenResponse createLoginTokenResponse() {
+        return LoginTokenResponse.builder()
+                .accessToken("testAccessToken")
+                .refreshToken("testRefreshToken")
+                .build();
     }
 
     private RequestFieldsSnippet tokenReissueRequestFieldsSnippet() {
@@ -85,87 +166,5 @@ class LoginControllerTest extends ControllerTest {
                 .refreshToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2NTI2MTE2MTF9.Hj9kuCtbxEQSdXgtGhKf0PnaXBNw4vtzeZ49fm24dREbRF7mOOw634ykk6aO0VjeuinikNVMI0xP5zURZj93OA")
                 .accessToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiLsl6zsp4Dsm5AiLCJhdXRoIjoiUk9MRV9VU0VSIiwicHJvdmlkZXJJZCI6IktBS0FPXzIwODcxNjM5NzUiLCJuaWNrbmFtZSI6IuynhOuLrOuemCIsImlkIjoxMiwiZXhwIjoxNjUxMjE4MjMzfQ.3_oRpdYdy4dHr9myPS8a032BNS0Acjt9SAqJ3E0yvWU19NFUN9nOQSZWge4cxX5kWucoZ-AAPKDnzcEzyfDEQA")
                 .build();
-    }
-
-    @WithMockAuthUser
-    @Test
-    void 구글_로그인_성공_테스트() throws Exception {
-        GoogleLoginDto googleLoginDto = new GoogleLoginDto("providerId", "test FcmToken");
-        LoginTokenResponse response = LoginTokenResponse.builder()
-                .accessToken("test123dsadsajd12131")
-                .refreshToken("testRefreshToken")
-                .build();
-
-        given(loginService.login(any())).willReturn(response);
-
-        mockMvc.perform(post("/login/google")
-                        .contentType(APPLICATION_JSON)
-                        .with(csrf())
-                        .content(objectMapper.writeValueAsString(googleLoginDto)))
-                .andExpect(status().isOk())
-                .andExpect(content().json(objectMapper.writeValueAsString(response)))
-                .andDo(document("google-login",
-                        setLoginRequestFields(),
-                        setLoginResponseFields()
-                ));
-    }
-
-    @WithMockAuthUser
-    @Test
-    void 네이버_로그인_성공_테스트() throws Exception {
-        NaverLoginDto naverLoginDto = new NaverLoginDto("providerId", "test FcmToken");
-        LoginTokenResponse response = LoginTokenResponse.builder()
-                .accessToken("test123dsadsajd12131")
-                .refreshToken("testRefreshToken")
-                .build();
-
-        when(loginService.login(any())).thenReturn(response);
-
-        mockMvc.perform(post("/login/naver")
-                        .contentType(APPLICATION_JSON)
-                        .with(csrf())
-                        .content(objectMapper.writeValueAsString(naverLoginDto)))
-                .andExpect(status().isOk())
-                .andExpect(content().json(objectMapper.writeValueAsString(response)))
-                .andDo(document("naverLogin-login",
-                        setLoginRequestFields(),
-                        setLoginResponseFields()
-                ));
-    }
-
-    @WithMockAuthUser
-    @Test
-    void 카카오_로그인_성공_테스트() throws Exception {
-        KakaoLoginDto kakaoLoginDto = new KakaoLoginDto("providerId", "testFcmToken");
-        LoginTokenResponse response = LoginTokenResponse.builder()
-                .accessToken("test123dsadsajd12131")
-                .refreshToken("testRefreshToken")
-                .build();
-
-        given(loginService.login(any())).willReturn(response);
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/login/kakao").contentType(APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(kakaoLoginDto))
-                        .with(csrf()))
-                .andExpect(status().isOk())
-                .andExpect(content().json(objectMapper.writeValueAsString(response)))
-                .andDo(document("login-login",
-                        setLoginRequestFields(),
-                        setLoginResponseFields()
-                ));
-    }
-
-    private RequestFieldsSnippet setLoginRequestFields() {
-        return requestFields(
-                fieldWithPath("providerId").type(JsonFieldType.STRING).description("유저의 카카오 고유 아이디"),
-                fieldWithPath("fcmToken").type(JsonFieldType.STRING).description("로그인한 기기의 fcmToken")
-        );
-    }
-
-    private ResponseFieldsSnippet setLoginResponseFields() {
-        return responseFields(
-                fieldWithPath("accessToken").type(JsonFieldType.STRING).description("로그인 성공 후 발급하는 accessToken"),
-                fieldWithPath("refreshToken").type(JsonFieldType.STRING).description("로그인 성공 후 발급하는 refreshToken")
-        );
     }
 }
