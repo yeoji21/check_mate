d07eaf13e6fb9261fbe3b8eca18a165d8d1e830d
여지원 <yooara0508@naver.com>
2023-06-20 23:18:08 +0900

    Refactor : ProgressCalculator

--- src/main/java/checkmate/common/util/ProgressCalculator.java
+++ src/main/java/checkmate/common/util/ProgressCalculator.java
@@ -1,25 +1,26 @@
 package checkmate.common.util;
 
-import lombok.experimental.UtilityClass;
+import static java.math.BigDecimal.ROUND_HALF_UP;
 
 import java.math.BigDecimal;
-
-import static java.math.BigDecimal.ROUND_HALF_UP;
+import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class ProgressCalculator {
+
     public static double calculate(int progressedCount, int totalCount) {
-        BigDecimal progressed = new BigDecimal(progressedCount);
-        BigDecimal total = new BigDecimal(totalCount);
-        if(existZero(progressed, total)) return 0;
-        else return getPercentage(progressed, total);
+        if (hasZero(progressedCount, totalCount)) {
+            return 0;
+        }
+        return divide(progressedCount, totalCount).doubleValue() * 100;
     }
 
-    private static boolean existZero(BigDecimal progressed, BigDecimal total) {
-        return total.compareTo(BigDecimal.ZERO) <= 0 || progressed.compareTo(BigDecimal.ZERO) <= 0;
+    private static BigDecimal divide(int progressedCount, int totalCount) {
+        return new BigDecimal(progressedCount).divide(new BigDecimal(totalCount), 3, ROUND_HALF_UP);
     }
 
-    private static double getPercentage(BigDecimal progressedCount, BigDecimal totalCount) {
-        return progressedCount.divide(totalCount, 3, ROUND_HALF_UP).doubleValue() * 100;
+    private static boolean hasZero(int progressedCount, int totalCount) {
+        return progressedCount <= 0 || totalCount <= 0;
     }
+
 }
