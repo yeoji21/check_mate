c809fcc90f5d29fed08fde3bbd9526eb99a0f9a5
여지원 <yooara0508@naver.com>
2023-06-24 00:06:14 +0900

    Chore : GoalQueryService

--- src/main/java/checkmate/common/cache/CacheHandler.java
+++ src/main/java/checkmate/common/cache/CacheHandler.java
@@ -2,6 +2,7 @@ package checkmate.common.cache;
 
 import java.util.List;
 
+// TODO: 2023/06/23 UserRepository 내부로 ?
 public interface CacheHandler {
 
     void deleteUserCaches(List<Long> userIds);
--- src/main/java/checkmate/goal/application/GoalQueryService.java
+++ src/main/java/checkmate/goal/application/GoalQueryService.java
@@ -3,35 +3,40 @@ package checkmate.goal.application;
 import checkmate.common.cache.CacheKey;
 import checkmate.exception.NotFoundException;
 import checkmate.exception.code.ErrorCode;
-import checkmate.goal.application.dto.response.*;
+import checkmate.goal.application.dto.response.GoalDetailInfo;
+import checkmate.goal.application.dto.response.GoalHistoryInfo;
+import checkmate.goal.application.dto.response.GoalScheduleInfo;
+import checkmate.goal.application.dto.response.OngoingGoalInfoResult;
+import checkmate.goal.application.dto.response.TodayGoalInfoResult;
 import checkmate.goal.infra.GoalQueryDao;
 import checkmate.mate.application.dto.response.GoalHistoryInfoResult;
 import checkmate.mate.infra.MateQueryDao;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Map;
-
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class GoalQueryService {
+
     private final GoalQueryDao goalQueryDao;
     private final MateQueryDao mateQueryDao;
 
     @Transactional(readOnly = true)
     public GoalDetailInfo findGoalDetail(long goalId) {
         return goalQueryDao.findDetailInfo(goalId)
-                .orElseThrow(() -> new NotFoundException(ErrorCode.GOAL_NOT_FOUND, goalId));
+            .orElseThrow(() -> new NotFoundException(ErrorCode.GOAL_NOT_FOUND, goalId));
     }
 
     @Cacheable(
-            value = CacheKey.ONGOING_GOALS,
-            key = "{#userId, T(java.time.LocalDate).now().format(@dateFormatter)}"
+        value = CacheKey.ONGOING_GOALS,
+        key = "{#userId, T(java.time.LocalDate).now().format(@dateFormatter)}"
     )
     @Transactional(readOnly = true)
     public OngoingGoalInfoResult findOngoingGoalInfo(long userId) {
@@ -42,12 +47,12 @@ public class GoalQueryService {
     @Transactional(readOnly = true)
     public GoalScheduleInfo findGoalPeriodInfo(long goalId) {
         return goalQueryDao.findGoalScheduleInfo(goalId)
-                .orElseThrow(() -> new NotFoundException(ErrorCode.GOAL_NOT_FOUND, goalId));
+            .orElseThrow(() -> new NotFoundException(ErrorCode.GOAL_NOT_FOUND, goalId));
     }
 
     @Cacheable(
-            value = CacheKey.TODAY_GOALS,
-            key = "{#userId, T(java.time.LocalDate).now().format(@dateFormatter)}"
+        value = CacheKey.TODAY_GOALS,
+        key = "{#userId, T(java.time.LocalDate).now().format(@dateFormatter)}"
     )
     @Transactional(readOnly = true)
     public TodayGoalInfoResult findTodayGoalInfo(long userId) {
@@ -58,16 +63,19 @@ public class GoalQueryService {
     @Transactional(readOnly = true)
     public GoalHistoryInfoResult findGoalHistoryResult(long userId) {
         List<GoalHistoryInfo> historyInfo = goalQueryDao.findGoalHistoryInfo(userId);
-        setMateNicknames(historyInfo);
+        Map<Long, List<String>> mateNicknames = mateQueryDao.findMateNicknames(
+            mapToGoalId(historyInfo));
+        historyInfo.forEach(setNicknamesToHistoryInfo(mateNicknames));
         return new GoalHistoryInfoResult(historyInfo);
     }
 
-    private void setMateNicknames(List<GoalHistoryInfo> historyInfo) {
-        Map<Long, List<String>> mateNicknames = mateQueryDao.findMateNicknames(getGoalIds(historyInfo));
-        historyInfo.forEach(info -> info.setMateNicknames(mateNicknames.get(info.getGoalId())));
+    private List<Long> mapToGoalId(List<GoalHistoryInfo> historyInfo) {
+        return historyInfo.stream().map(GoalHistoryInfo::getGoalId).toList();
     }
 
-    private List<Long> getGoalIds(List<GoalHistoryInfo> historyInfo) {
-        return historyInfo.stream().map(info -> info.getGoalId()).toList();
+    private Consumer<GoalHistoryInfo> setNicknamesToHistoryInfo(
+        Map<Long, List<String>> mateNicknames) {
+        return historyInfo -> historyInfo.setMateNicknames(
+            mateNicknames.get(historyInfo.getGoalId()));
     }
 }
